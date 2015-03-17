eed0aa8152d00f3ef18b61ec8329fa17d1d054c2
Nathan Corvino <nathan@corvino.com>
2015-03-17 15:18:21 -0500

    Initial project generated by Xcode 6.3 Beta 3.

---
+++ LayerExplorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LayerExplorer
+//
+//  Created by Nathan Corvino on 3/17/15.
+//  Copyright (c) 2015 Nathan Corvino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LayerExplorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LayerExplorer
+//
+//  Created by Nathan Corvino on 3/17/15.
+//  Copyright (c) 2015 Nathan Corvino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
